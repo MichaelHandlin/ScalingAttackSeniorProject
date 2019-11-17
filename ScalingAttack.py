0303b4c1b5245790ecf39d2fdef5e654215528d3
Michael Handlin <32713484+MichaelHandlin@users.noreply.github.com>
2019-11-17 12:05:20 -0500

    Creating image from channel matrices
    It's messy right now because we needed to figure out how to make an image from channel matrices and I noticed that we hadn't gotten to that yet.

--- ScalingAttack.py
+++ ScalingAttack.py
@@ -20,30 +20,66 @@ def create_attack_image(src_img, tgt_img, scale_func):
     m, n = src_img.size
     m_prime, n_prime = tgt_img.size
     CL, CR = get_coefficients(m, n, m_prime, n_prime)
-    #Get color matrices for each image
-    tgt_red, tgt_green, tgt_blue, tgt_alpha =
-    src_red, src_green, src_blue = get_color_matrices(src_img)
+    #Get color matrices for each
+    src_img.convert('RGB')  # Remove alpha channel
+    tgt_img.convert('RGB')  # Remove Alpha Channel
+    tgt_red, tgt_green, tgt_blue = tgt_img.split()
+    src_red, src_green, src_blue = src_img.split()
+    tgt_red_arr = np.array(tgt_red)
+    tgt_blue_arr = np.array(tgt_blue)
+    tgt_green_arr = np.array(tgt_green)
+    tgt_arrs = [tgt_red_arr, tgt_blue_arr, tgt_green_arr]
+    src_red_arr = np.array(src_red)
+    src_red_scaled = np.dot(np.dot(CL, src_red_arr), CR)
+    src_blue_arr = np.array(src_blue)
+    src_blue_scaled = np.dot(np.dot(CL, src_blue_arr), CR)
+    src_green_arr = np.array(src_green)
+    src_green_scaled = np.dot(np.dot(CL, src_green_arr), CR)
+    src_arrs = [src_red_arr, src_blue_arr, src_green_arr]
+    #(Image.fromarray(np.dot(np.dot(CL, src_green_arr), CR)).convert("RGB")).save("test.jpg")
+    #Successfully makes a scaled image.
+    atk_img_arr = np.zeros((n, m, 3), dtype=np.uint8)
+    CL_inv, CR_inv = _get_inv_coeff(m, n, m_prime, n_prime)
+    tgt_red_scaled = np.dot(np.dot(CL_inv, tgt_red_arr), CR_inv)
+    (Image.fromarray(src_red_scaled).convert("RGB")).save("tgt_red_scaled.png")
+    tgt_blue_scaled = np.dot(np.dot(CL_inv, tgt_blue_arr), CR_inv)
+    tgt_green_scaled = np.dot(np.dot(CL_inv, tgt_green_arr), CR_inv)
+    for y in range(atk_img_arr.shape[0]):
+        for x in range(atk_img_arr.shape[1]):
+            atk_img_arr[y][x][0] = tgt_red_scaled[y][x]
+            atk_img_arr[y][x][1] = tgt_green_scaled[y][x]
+            atk_img_arr[y][x][2] = tgt_blue_scaled[y][x]
+    test_image = ImageChops.add(src_img, Image.fromarray(atk_img_arr))
+    test_image = test_image.resize((n_prime, m_prime))
+    test_image.save("test1.png")
+    #ImageChops.add(Image.fromarray(atk_img_arr), tgt_img).save("test.jpg")
+    atk_arrs = []
+    #for i in range(3):
+    #    atk_arrs[i] = Image.fromarray()
 
+
+    #tgt_blue_img = Image.fromarray(tgt_blue)
     # Create temp image to initialize delta v1
-    temp_img = Image.fromarray(CL * np.array(tgt_img) * CR) #operands could not be broadcast together with shapes (900,256) (256,256,3)
+    #temp_img = Image.fromarray(CL * np.array(tgt_img) * CR) #operands could not be broadcast together with shapes (900,256) (256,256,3)
     # are those color channels?
     # Missing a step here to get the shapes to be compatible.
     # np.array(Image.open(src), np.float)
-    temp_img.show()
-    temp_tgt_img = ImageChops.add(src_img, Image.fromarray(CL * np.array(tgt_img) * CR))
-    temp_tgt_img.show()
-    temp_delta_v1 = np.array(temp_tgt_img) - np.array(src_img)
-    delta_v1 = np.zeros(shape=(m, n_prime))
-    S_mnp = np.array(src_img.resize((m, n_prime), scale_func))
-    for col in range(n_prime):
-        delta_v1[:, col] = get_perturbation(S_mnp[:, col], tgt_img[:, col], temp_delta_v1, CL)
-    A_mnp = S_mnp + delta_v1
+    # temp_img.show()
+    #temp_tgt_img = ImageChops.add(src_img, Image.fromarray(CL * np.array(tgt_img) * CR))
+    #temp_tgt_img.show()
+    #temp_delta_v1 = np.array(temp_tgt_img) - np.array(src_img)
+    #delta_v1 = np.zeros(shape=(m, n_prime))
+    #S_mnp = np.array(src_img.resize((m, n_prime), scale_func))
+    #for col in range(n_prime):
+    #    delta_v1[:, col] = get_perturbation(S_mnp[:, col], tgt_img[:, col], temp_delta_v1, CL)
+    #A_mnp = S_mnp + delta_v1
 
-    delta_h1 = np.zeros(shape=(m, n))
-    for row in range(m):
-        delta_h1[row, :] = get_perturbation(src_img[row, :], A_mnp[row, :], CR)
-    A_mn = src_img + delta_h1
-    return Image.fromarray(A_mn)
+    #delta_h1 = np.zeros(shape=(m, n))
+    #for row in range(m):
+    #    delta_h1[row, :] = get_perturbation(src_img[row, :], A_mnp[row, :], CR)
+    #A_mn = src_img + delta_h1
+    #return Image.fromarray(A_mn)
+    return Image.fromarray(CL)
 
 
 def get_coefficients(m, n, m_prime, n_prime):
@@ -51,29 +87,43 @@ def get_coefficients(m, n, m_prime, n_prime):
     I_nn = np.identity(n)
 
     IN_max = 255  # Typical maximum pixel value for most image formats
-    D_mpm = Image.fromarray(np.array(I_mm)).resize((m_prime, m))  # D_m'*m
+    D_mpm = Image.fromarray(np.array(I_nn * IN_max)).resize((n, n_prime))  # D_m'*m
     CL = np.array(D_mpm) / IN_max
 
     # Find CR
     S_arr = np.array(I_nn)
-    D_nnp = Image.fromarray(np.array(I_nn)).resize((n, n_prime))  # D_n*n'
+    D_nnp = Image.fromarray(np.array(I_mm * IN_max)).resize((m_prime, m))  # D_n*n'
     CR = np.array(D_nnp) / IN_max
 
     return CL, CR
 
+def _get_inv_coeff(m, n, m_prime, n_prime):
+    I_mm = np.identity(m_prime)
+    I_nn = np.identity(n_prime)
+
+    IN_max = 255  # Typical maximum pixel value for most image formats
+    D_mpm = Image.fromarray(np.array(I_mm * IN_max)).resize((n_prime, n))  # D_m'*m
+    CL = np.array(D_mpm) / IN_max
+
+    # Find CR
+    S_arr = np.array(I_nn)
+    D_nnp = Image.fromarray(np.array(I_nn * IN_max)).resize((m, m_prime))  # D_n*n'
+    CR = np.array(D_nnp) / IN_max
+
+    return CL, CR
 
 # if columnwise,
 #   S is column s[:, col], T is T[:, col], CX is CL.
 # if row-wise
 #   S is row S[row, :], T is A*[row, :], CX is CR.
-def get_perturbation(S, T, CX, delta_1, obj='min'):
+def get_perturbation(S, T, CX, delta_1, obj='min', IN_max=255, epsilon=.001):
     global m_prime
     # Ensure constraints are met
     j = cvx.Variable(2)
     delta_1T = np.transpose(delta_1)
     function = cvx.Minimize(delta_1T[:, j] * np.identity(m_prime) * delta_1[:, j])
-    #problem = cvx.Problem(function, [0 <= T[:, j], T[:, j] <= IN_max, cvx.norm(CX*A[:, j] - T[:, j], "inf")] < epsilon *IN_max)
-    problem = cvx.Problem(function, [0 <= j, j < len(S)])
+    problem = cvx.Problem(function, [0 <= T[:, j], T[:, j] <= IN_max, cvx.norm(CX*A[:, j] - T[:, j], "inf")] < epsilon *IN_max)
+    #problem = cvx.Problem(function, [0 <= j, j < len(S)])
     return problem.solve()
 
 
